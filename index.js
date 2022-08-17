d36c41a9608ccfa2da161c246adf555cb6bd4efa
abdullah ashiq <abdullahashiq1996@gmail.com>
2022-08-17 10:59:28 +0600

    JWT token failed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express();
@@ -15,17 +16,45 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+
 async function run(){
     try{
         await client.connect();
         const serviceCollection = client.db('doctors-portal').collection('services');
         const bookingCollection = client.db('doctors-portal').collection('bookings');
+        const userCollection = client.db('doctors-portal').collection('users');
+
 
         app.get('/service', async(req, res) =>{
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        });
+
+        app.put('/user/admin/:email', async(req, res) =>{
+            const email = req.params.email;
+           
+            const filter = {email: email};
+            const updateDoc = {
+                $set: {role:'admin'},
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+        res.send(result);
+        });
+
+        app.put('/user/:email', async(req, res) =>{
+            const email = req.params.email;
+            const user  = req.body;
+            const filter = {email: email};
+            const options = {upsert: true};
+            const updateDoc = {
+                $set: user,
+        };
+        const result = await userCollection.updateOne(filter, updateDoc, options);
+        // const accessToken = jwt.sign({email: email}, tokenkey, { expiresIn: '1h' });
+        res.send(result);
         });
 
 
@@ -49,21 +78,27 @@ async function run(){
      
 
       res.send(services);
-    })
+    });
         /**
          * API Naming Convention
          * app.get('/booking') // get all bookings in this collection, or get then one more or filter or query
          * app.get('/booking/:id') get a specific booking
          * app.post('/booking') // add a new booking
          * app.patch('/booking/:id') // update a new booking
+         * app.put('/booking/:id')  // upsert ==> update(if exist) or insert (if dosen't exist)
          * app.delete('/booking/:id')
-         */
+        **/
 
         app.get('/booking', async(req, res) =>{
             const patient = req.query.patient;
             const query = {patient: patient};
             const bookings = await bookingCollection.find(query).toArray();
             res.send(bookings);
+        })
+
+        app.get('/user', async(req, res) =>{
+            const users = await userCollection.find().toArray();
+            res.send(users)
         })
 
         app.post('/booking', async(req, res) =>{
